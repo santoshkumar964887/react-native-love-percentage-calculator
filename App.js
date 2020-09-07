2340566d161824b977d046cc39f309de7e99b2ee
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-07 17:07:10 +0530

    Page desigining has done

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,TextInput } from 'react-native';
-
-export default function App() {
+import { StyleSheet, Text, View} from 'react-native';
+import { Appbar,TextInput, Button  } from 'react-native-paper';
+class  App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      Male:"",
+      Female:""
+    }
+  }
+  handlepress=()=>{
+    console.log("santosh")
+  }
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>
-        santosh kumar96
-        
-
-        
-      </Text>
-     
-      <StatusBar style="auto" />
+    <View>
+     <Appbar.Header>
+      <Appbar.Content title="Love Calculator" subtitle="find Love compatibility & chances of successful love relationship" />
+    </Appbar.Header> 
+    <TextInput
+   
+      label="Enter Male Name"
+      value={this.state.text}
+      onChangeText={text =>console.log(text)
+      }
+    />
+      <TextInput
+      label="Enter Female Name"
+      value={this.state.Female}
+      onChangeText={text =>this.setState({Female:text})}
+    />
+     <Button style={{margin:12}} icon="camera" mode="contained" onPress={this.handlepress}>Love Calculator</Button>
+      
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -25,3 +46,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+export default App;
