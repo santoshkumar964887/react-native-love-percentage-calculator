42dd5a83380de22a2b10c2f92b2dc8989ef31469
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-07 12:57:35 +0530

    setup files

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        santosh kumar96
+        
+
+        
+      </Text>
+     
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
